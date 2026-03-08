de7e0743fba6ac3008a476b0a71b22dd57f288d0
clv07 <lchandra@ucsd.edu>
2026-03-08 14:55:08 -0700

    Updated buildtree with Liang Yuan

--- src/buildtree.cu
+++ src/buildtree.cu
@@ -6,19 +6,36 @@
 #define BLOCKSIZE 256
 #define NUMBLOCKS 8
 
+
+__global__ void initActive(uint32_t d_size, uint32_t* active) {
+    for (uint32_t i=bs*bx+tx; i < d_size; i+=bs*gs) {
+        active[i] = 1;
+    }
+}
+
+__global__ void blockReduction (uint32_t* d_min_ij, double* d_min_dst) {
+    // parallel reduction across block
+    for (int s=NUMBLOCKS/2; s>0; s>>=1) { 
+        if (bx < s) {
+            if(d_min_dst[bx].dst < d_min_dst[bx+s].dst) {
+                d_min_dst[bx] = d_min_dst[bx+s];
+                d_min_ij[bx] = d_min_ij[bx+s];        
+            }
+        } 
+    }
+}
+
 /**
  * Building phylogenetic tree on GPU
  */
- __global__ void buildTreeOnCPU(uint32_t d_size, double *mat, uint32_t *d_nodes, double* u_i, uint32_t* d_min_ij, double* d_min_dst){
+ __global__ void buildTreeOnCPU(uint32_t d_size, double *mat, double* u_i, uint32_t* active, uint32_t* d_min_ij, double* d_min_dst,  double *d_merged_dst){
     // Kernel Configuration
     int bx = blockIdx.x;
     int tx = threadIdx.x;
     int bs = blockDim.x;
     int gs = gridDim.x;
-
-    uint32_t u_len = d_size*(d_size-1)/2;
     
-    struct red_dst {
+    struct rdc_dst {
         uint32_t index_i;
         uint32_t index_j;
         double dst;
@@ -27,36 +44,35 @@
 
     // one grid for one distance matrix
     // 1. for each leaf compute u_i = summation from j!= i to N, (d_ij) / (N-2)
-    for (uint32_t i = bs*bx+tx; i<N; i+=bs*gs) {
+    for (uint32_t i = bs*bx+tx; i<d_size; i+=bs*gs) {
+        if (active[i] == 0) continue;
         double sum_ui = 0.0;
         for (uint32_t j=0; j<i; j++){
+            if (active[j] == 0) continue;
             uint32_t index = i*(i-1)/2+j;
-            if (d_nodes[index] == 0) continue; // skip inactive nodes
             double d_ij = mat[index]; 
             sum_ui += d_ij;
         }
-        u_i[i] = sum_ui / (N-2);
+        u_i[i] = sum_ui / (d_size-2);
     }
 
-    // initialize reduction
-    uint32_t i=tx; 
-    if(i<BLOCKSIZE; i++) {
-        reduction[i].index_i = -1;
-        reduction[i].index_j = -1;
-        reduction[i].dst = DBL_MAX;
-    }
+    // initialize reduction within blocks 
+    reduction[tx].index_i = -1;
+    reduction[tx].index_j = -1;
+    reduction[tx].dst = DBL_MAX;
 
     // 2. find leaf pair i and j for which d_ij - u_i - u_j is minimum 
-    uint32_t i = tx;
-    if (i < N) {
+    for (uint32_t i = bs*bx+tx; i<d_size; i+=bs*gs) {
+        if (active[i] == 0) continue;
         for (uint32_t j=0; j<i; j++) {
+            if (active[j] == 0) continue;
             uint32_t index = i*(i-1)/2+j;
             // if (d_nodes[index] == 0) continue; // skip inactive nodes
-            double leaf = mat[index] - u_i[i] - u_j[j];
-            if (leaf < reduction[i].dst) { 
-                reduction[i].dst = leaf;
-                reduction[i].index_i = i;
-                reduction[i].index_j = j;
+            double leaf = mat[index] - u_i[i] - u_i[j];
+            if (leaf < reduction[tx].dst) { 
+                reduction[tx].dst = leaf;
+                reduction[tx].index_i = i;
+                reduction[tx].index_j = j;
             }
         }
     }
@@ -70,25 +86,34 @@
     }
 
     if (tx == 0) {
-        d_min_ij[bx][0] = reduction[tx].index_i;
-        d_min_ij[bx][1] = reduction[tx].index_j;
+        d_min_ij[2*bx] = reduction[tx].index_i;
+        d_min_ij[2*bx+1] = reduction[tx].index_j;
         d_min_dst[bx] = reduction[tx].dst;
     }
     __syncthreads();
 
+    // Problem 1
     // parallel reduction across block
-    for (int s=BLOCKSIZE/2; s>0; s>>=1) { 
-        if (tx < s) {
-            if(reduction[tx].dst > reduction[tx+s].dst) reduction[tx] = reduction[tx+s];
-        }
+    for (int s=NUMBLOCKS/2; s>0; s>>=1) { 
+        if (bx < s) {
+            if(d_min_dst[bx].dst < d_min_dst[bx+s].dst) {
+                d_min_dst[bx] = d_min_dst[bx+s];
+                d_min_ij[bx] = d_min_ij[bx+s];        
+            }
         __syncthreads();
     } 
 
     // 3. Join i and j to form a common node (ij) such that
     // the distance of i from the (ij), d_i(ij) = 0.5 * (d_ij + (u_i-u_j)) and 
     // the distance of j from the (ij), d_j(ij) = d_ij - d_i(ij)
-    double d_i_ij =  0.5 * (min_dst + (u_i[min_i] - u_i[min_j]));
+    double min_dst = d_min_dst[0];
+    double min_i = d_min_ij[0][0]; // Problem 2
+    double min_j = d_min_ij[0][1];
+    double d_i_ij = 0.5 * (min_dst + (u_i[min_i] - u_i[min_j]));
     double d_j_ij = mid_dst - d_i_ij;
+
+    active[min_i] = 1; // which indicate new nodes is active
+    active[min_j] = 0; // merged 
 
     // adjustment for negative branch length
     if (d_i_ij < 0.0) {
@@ -100,22 +125,29 @@
         d_i_ij = (min_dst < 0.0)? 0.0 : d_ij;
     }
 
+    // copy distances of i and j from ij to variable in global memory
+    d_merged_dst[0] = d_i_ij;
+    d_merged_dst[1] = d_j_ij;
+
+    // Update distance matrix
     // 4. Treat (ij) as a new tip, ignoring previous tips i and j
     // 5. Distance of (ij) from other tips k, d_k(ij) = (d_ik+d_jk-d_ij) * 0.5 
     uint32_t a = std::min(min_i, min_j);
     uint32_t b = std::max(min_i, min_j);
 
     // set new merged distance for (a, k) for all tips k 
-    for (uint32_t k=bs*bx+tx; k<N; k+=gs*bs) {
+    for (uint32_t k=bs*bx+tx; k<d_size; k+=gs*bs) {
+        if (active[k] == 0) continue;
         uint32_t ak_index = a*(a-1)/2+k;
         uint32_t bk_index = b*(b-1)/2+k;
-        if (k != a && k != b && d_nodes[k] == 1){
-             double new_d = (mat[ak_index] + mat[bk_index] - mat[min_dst]) * 0.5;
+        if (k != a && k != b){
+             double new_d = (mat[ak_index] + mat[bk_index] - min_dst) * 0.5;
              mat[ak_index] = (new_d < 0.0)? 0.0: new_d; // adjust for negative branch length 
         }
-        d_nodes[bk_index] = 0; // set tips b to inactive
     }
 
+    d_size = d_size - 1; // Problem 3
+    
  }
 
 
@@ -138,7 +170,7 @@
     transferMat2Device();
 
     // 3. Perform building tree on GPU
-    buildTreeOnCPU<<<numBlocks, blockSize>>>(d_size, mat, d_nodes, u_i, d_min);
+    buildTreeOnCPU<<<numBlocks, blockSize>>>(d_size, mat, d_nodes, u_i, d_min, d_minj);
     
 
     // 4. Transfer the final merged nodes from device
@@ -172,15 +204,8 @@ void GpuTree::allocateMem() {
         exit(1);
     }
 
-    // 3. allocate memory to the final merged nodes array 
-    err = cudaMalloc(&d_nodes, h_size * sizeof(double));
-    if (err != cudaSuccess) {
-        fprintf(stderr, "GPU_ERROR: %s (%s)\n", cudaGetErrorString(err), cudaGetErrorName(err));
-        exit(1);
-    }
-
-    // 4. allocate memory to the  
-    err = cudaMalloc(&d_leftovers, 3 * sizeof(uint32_t));
+    // 3. allocate memory to nodes status array 
+    err = cudaMalloc(&active, h_size * sizeof(uint32_t));
     if (err != cudaSuccess) {
         fprintf(stderr, "GPU_ERROR: %s (%s)\n", cudaGetErrorString(err), cudaGetErrorName(err));
         exit(1);
@@ -194,14 +219,21 @@ void GpuTree::allocateMem() {
     }
 
     // 6. allocate struct for min_i, min_j
-    err = cudaMalloc(&d_min_ij, 2 * sizeof(uint32_t));
+    err = cudaMalloc(&d_min_ij, 2 * NUMBLOCKS * sizeof(uint32_t));
     if (err != cudaSuccess) {
         fprintf(stderr, "GPU_ERROR: %s (%s)\n", cudaGetErrorString(err), cudaGetErrorName(err));
         exit(1);
     }
 
     // 7. allocate struct for min_dst
-    err = cudaMalloc(&d_min_dst, sizeof(double));
+    err = cudaMalloc(&d_min_dst, NUMBLOCKS * sizeof(double));
+    if (err != cudaSuccess) {
+        fprintf(stderr, "GPU_ERROR: %s (%s)\n", cudaGetErrorString(err), cudaGetErrorName(err));
+        exit(1);
+    }
+
+    // 8. allocate struct for distance of min i and min j from merged ij
+    err = cudaMalloc(&d_merged_dst, 2 * sizeof(double));
     if (err != cudaSuccess) {
         fprintf(stderr, "GPU_ERROR: %s (%s)\n", cudaGetErrorString(err), cudaGetErrorName(err));
         exit(1);
@@ -214,12 +246,14 @@ Transfer distance matrix data and size to device
 void GpuTree::transferMat2Device() {
     cudaError_t err;
 
+    // Problem 4
     err = cudaMemcpy(mat, data, (h_size*(h_size-1)/2) * sizeof(double), cudaMemcpyHostToDevice);
     if (err != cudaSuccess) {
         fprintf(stderr, "GPU_ERROR: %s (%s)\n", cudaGetErrorString(err), cudaGetErrorName(err));
         exit(1);
     }
 
+    // Problem 5
     err = cudaMemcpy(d_size, h_size, sizeof(uint32_t), cudaMemcpyHostToDevice);
     if (err != cudaSuccess) {
         fprintf(stderr, "GPU_ERROR: %s (%s)\n", cudaGetErrorString(err), cudaGetErrorName(err));
@@ -234,6 +268,13 @@ std::vector<uint32_t> GpuTree::transferTree2Host() {
     std::vector<uint32_t> nodes;
 
     cudaError_t err = cudaMemcpy(h_nodes, d_nodes, size, cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "GPU_ERROR: %s (%s)\n", cudaGetErrorString(err), cudaGetErrorName(err));
+        exit(1);
+    }
+
+    // transfer merged nodes distance from device to host
+    cudaError_t err = cudaMemcpy(h_merged_dst, d_merged_dst, 2 * sizeof(double), cudaMemcpyDeviceToHost);
     if (err != cudaSuccess) {
         fprintf(stderr, "GPU_ERROR: %s (%s)\n", cudaGetErrorString(err), cudaGetErrorName(err));
         exit(1);
--- src/buildtree.cuh
+++ src/buildtree.cuh
@@ -7,7 +7,7 @@ struct GpuTree {
     uint32_t h_size;
     uint32_t *h_nodes;
     uint32_t *h_min_ij;
-    double *h_min_dst;
+    double *h_merged_dst;
 
     // GPU memory
     double *mat;  // input
@@ -16,10 +16,10 @@ struct GpuTree {
 
     uint32_t *d_min_ij; // min i and min j packed into one
     double *d_min_dst; // min distance
+    double *d_merged_dst; // [0] distance from i to ij
+                         // [1] distance from j to ij
 
-    uint32_t *d_nodes;  // output
-    uint32_t *d_leftovers; // output
-
+    uint32_t *active;  // active nodes that need to merge
 
     void allocateMem();
     void transferMat2Device();
